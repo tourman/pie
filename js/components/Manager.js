6b828407d3cc50f13ebd7ea5640e0527912ef5d2
tourman <8002615@gmail.com>
2018-02-13 10:12:23 +0700

    Manager: base events

--- js/components/Manager.js
+++ js/components/Manager.js
@@ -9,6 +9,32 @@ class Manager extends Component {
       description: '',
       rate: ''
     };
+
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeRate = this.onChangeRate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChangeDescription(event) {
+    event.preventDefault();
+    this.setState({
+      description: event.target.value
+    });
+  }
+
+  onChangeRate(event) {
+    event.preventDefault();
+    this.setState({
+      rate: event.target.value
+    });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      description: '',
+      rate: ''
+    });
   }
 
   render() {
@@ -17,13 +43,17 @@ class Manager extends Component {
         <h3 className="manager__title">
           {this.state.title}
         </h3>
-        <form className="manager__form manager-form">
+        <form
+          className="manager__form manager-form"
+          onSubmit={this.onSubmit}
+        >
           <input
             type="text"
             className="manager-form__description"
             name="description"
             value={this.state.description}
             placeholder="Description"
+            onChange={this.onChangeDescription}
           />
           <input
             type="text"
@@ -31,6 +61,7 @@ class Manager extends Component {
             name="rate"
             value={this.state.rate}
             placeholder="Rate"
+            onChange={this.onChangeRate}
           />
           <button
             type="submit"
