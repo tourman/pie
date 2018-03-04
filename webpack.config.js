5d1e11fa5e900e13af663b3d5076ce17202926b9
tourman <8002615@gmail.com>
2018-03-04 17:03:40 +0700

    TS: successfull launch

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,44 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-	entry: [
-		path.join(__dirname, '/js/index.js')
-	],
-	output: {
-		path: path.join(__dirname, '/js/'),
-		filename: 'bundle.js',
-		publicPath: '/js/'
-	},
-	devtool: 'eval',
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.js?$/,
-				exclude: /node_modules/,
-				loaders: ['babel-loader']
-			}
-		]
-	},
-	devServer: {
-		hot: true,
-		inline: true,
-		contentBase: '.'
-	}
+  entry: [
+    path.join(__dirname, '/js/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/js/'),
+    filename: 'bundle.js',
+    publicPath: '/js/'
+  },
+  resolve: {
+    extensions: [
+      '.ts',
+      '.tsx',
+      '.js',
+      '.jsx'
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        use: {
+          loader: 'awesome-typescript-loader'
+        }
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    inline: true,
+    contentBase: '.'
+  }
 }
