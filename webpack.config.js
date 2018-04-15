79f01ad9aee16197299221c8a3f9b76a3201c1a3
tourman <8002615@gmail.com>
2018-04-15 10:35:45 +0300

    file refactoring: webpack.config.js indentation

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,30 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-	entry: [
-		path.join(__dirname, '/js/index.js')
-	],
-	output: {
-		path: path.join(__dirname, '/js/'),
-		filename: 'bundle.js',
-		publicPath: '/js/'
-	},
-	devtool: 'eval',
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.js?$/,
-				exclude: /node_modules/,
-				loaders: ['babel-loader']
-			}
-		]
-	},
-	devServer: {
-		hot: true,
-		inline: true,
-		contentBase: '.'
-	}
+  entry: [
+    path.join(__dirname, '/js/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/js/'),
+    filename: 'bundle.js',
+    publicPath: '/js/'
+  },
+  devtool: 'eval',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    inline: true,
+    contentBase: '.'
+  }
 }
